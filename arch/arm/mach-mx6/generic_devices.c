f9e9926e9d2e8092f9af32825da83b6ea6ff61da
raymond.wang <raymond1860@gmail.com>
2013-07-18 16:43:00 +0800

    IMX-16,Add pn544 driver skeleton in kernel.

--- arch/arm/mach-mx6/generic_devices.c
+++ arch/arm/mach-mx6/generic_devices.c
@@ -1,4 +1,6 @@
+#include <linux/i2c.h>
 #include <linux/w1-gpio.h>
+#include <linux/pn544.h>
 #include "generic_devices.h"
 
 
@@ -14,4 +16,28 @@ struct platform_device *__init generic_add_device_w1(
 	return imx_add_platform_device("w1-gpio", -1,
 			NULL, 0, pdata, sizeof(*pdata));
 }
+
+
+static struct pn544_i2c_platform_data pn544_pdata = {
+	.irq_gpio = 0,//NFC_IRQ,
+	.ven_gpio = 0,//NFC_EN,
+	.firm_gpio = 0,//NFC_FIRM,
+};
+
+
+static struct i2c_board_info pn544_device __initdata = {
+	I2C_BOARD_INFO("pn544", 0x28),
+	//.irq = IRQ_EINT(16),
+	.platform_data = &pn544_pdata,
+};
+struct platform_device *__init generic_add_device_pn544(
+		int bus,int irq,int ven,int fw)
+{
+	pn544_pdata.irq_gpio = irq;
+	pn544_pdata.ven_gpio = ven;
+	pn544_pdata.firm_gpio = fw;
+
+	return i2c_register_board_info(bus, &pn544_device,1);
+}
+
 
--- arch/arm/mach-mx6/generic_devices.h
+++ arch/arm/mach-mx6/generic_devices.h
@@ -4,4 +4,6 @@
 #define generic_add_w1(gpio)	\
 	generic_add_device_w1(gpio)
 
+#define generic_add_pn544(bus,irq,ven,fw) \
+	generic_add_device_pn544(bus,irq,ven,fw)
 
