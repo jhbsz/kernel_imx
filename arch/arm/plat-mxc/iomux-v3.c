17f5304e3ef093eca9fcf6ec9ade624565439a1b
raymond.wang <raymond1860@gmail.com>
2013-08-02 13:58:55 +0800

    IMX-29,enable all gpio SION bit to read it back of output state.

--- arch/arm/plat-mxc/iomux-v3.c
+++ arch/arm/plat-mxc/iomux-v3.c
@@ -31,6 +31,14 @@
 
 static void __iomem *base;
 
+#ifdef CONFIG_DEBUG_FS
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/debugfs.h>
+static struct dentry *debugfs_iomux;
+static int iomux_count;
+#endif
+
 /*
  * configures a single pad in the iomuxer
  */
@@ -142,7 +150,103 @@ void mxc_iomux_set_specialbits_register(u32 pad_addr, u32 value, u32 mask)
 }
 EXPORT_SYMBOL(mxc_iomux_set_specialbits_register);
 
+#ifdef CONFIG_DEBUG_FS
+
+static int iomux_open_file(struct inode *inode, struct file *file)
+{
+	file->private_data = inode->i_private;
+	return 0;
+}
+
+static ssize_t  iomux_read_file(struct file *file, char __user *user_buf,
+			       size_t count, loff_t *ppos)
+{
+	void __iomem *iomux_base = file->private_data;
+	ssize_t ret=0;
+	int i,step=4;
+	char *values = kmalloc(((iomux_count+PAGE_SIZE-1) & (~ (PAGE_SIZE-1))), GFP_KERNEL);
+	char *buf = kmalloc(10*PAGE_SIZE, GFP_KERNEL);	
+	for(i=0;i<iomux_count;i++){
+		values[i] = __raw_readl(iomux_base+i*step);
+	}
+	//now print to page buffer	
+	ret += sprintf(buf+ret,"iomux register dumper\n");			
+	for(i=0;i<iomux_count;i++){
+		if(0==i%4)
+			ret += sprintf(buf+ret,"\n0x%04x: ",i*step);			
+		ret += sprintf(buf+ret,"%04x ",values[i]);
+	}
+	
+	ret += sprintf(buf+ret,"\n");
+	if (ret >= 0)
+		ret = simple_read_from_buffer(user_buf, count, ppos, buf, ret);
+	kfree(buf);
+	kfree(values);
+	return ret;
+}
+
+static ssize_t  iomux_write_file(struct file *file,
+		const char __user *user_buf, size_t count, loff_t *ppos)
+{
+	void __iomem *iomux_base = file->private_data;
+	char buf[32];
+	int buf_size;	
+	int step=4;
+	char *start = buf;
+	unsigned long reg, value;
+	
+
+	buf_size = min(count, (sizeof(buf)-1));
+	if (copy_from_user(buf, user_buf, buf_size))
+		return -EFAULT;
+	buf[buf_size] = 0;
+
+	while (*start == ' ')
+		start++;
+	reg = simple_strtoul(start, &start, 16);
+	if (reg >= (iomux_count*step) )
+	{
+		printk("address out of dev io space\n");
+		return -EINVAL;
+	}
+	while (*start == ' ')
+		start++;
+	if (strict_strtoul(start, 16, &value))
+		return -EINVAL;
+	//permission granted?
+	printk("write %lx to %lx\n",value,reg);
+	__raw_writel(value,(void __iomem *)(iomux_base+reg));
+
+	return buf_size;
+}
+
+
+static const struct file_operations iomux_fops = {
+	.open =  iomux_open_file,
+	.read =  iomux_read_file,
+	.write = iomux_write_file,
+};
+
+#endif
 void mxc_iomux_v3_init(void __iomem *iomux_v3_base)
 {
 	base = iomux_v3_base;
 }
+
+static int __init iomux_late_init(void){
+#ifdef CONFIG_DEBUG_FS
+#ifdef CONFIG_SOC_IMX6Q
+	if(cpu_is_mx6q()){
+		iomux_count = 0x094C/4+1;//from RM
+		printk("iomux reg count = %d\n",iomux_count);
+	}
+#endif
+	debugfs_iomux = debugfs_create_file("iomux", 0644,
+					 NULL, base,
+					 &iomux_fops);	
+
+#endif
+	return 0;
+}
+late_initcall(iomux_late_init);
+
